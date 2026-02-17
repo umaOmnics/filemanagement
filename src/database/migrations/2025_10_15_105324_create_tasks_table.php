8ad32311b4735c23b92855975119be70bddc2570
umaOmnics <yandava@omnics.in>
2026-02-17 12:36:32 +0000

    auto update debug

--- src/database/migrations/2025_10_15_105324_create_tasks_table.php
+++ src/database/migrations/2025_10_15_105324_create_tasks_table.php
@@ -28,7 +28,6 @@ return new class extends Migration
                 $table->charset = 'utf8mb4';
                 $table->collation = 'utf8mb4_general_ci';
             });
-//        }
     }
 
     /**
